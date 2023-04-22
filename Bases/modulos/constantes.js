8edeb02b179bf87cb8f8c73d84607d54ca36ac44
MEmersonxdd <memersonxdd@gmail.com>
2023-04-22 02:33:03 -0500

    modulos :0

---
+++ Bases/modulos/constantes.js
@@ -0,0 +1,13 @@
+
+export const PI = Math.PI;
+
+//export default const dos = 2; // no se puede, pilla:
+
+const dos = 2;
+export default dos;
+
+export function saludar(){
+
+    console.log("Holi bb por qué tan solo ?")
+
+}
---
+++ Bases/modulos/modulos1.js
@@ -0,0 +1,12 @@
+
+import { PI } from "./constantes.js";
+import {aritmetica as art} from "./aritmetica.js";
+
+
+
+console.log("Modulos 1");
+
+console.log(PI);
+
+console.log(art.sumar(5,10));
+console.log(art.restar(15,5));
